55e802f888aef339cb291217bd028a005ca5c8a7
hezhilong <royhe.he@chofn.com>
2019-01-07 16:51:08 +0800

    添加随机密码生成

--- web-common/src/main/java/com/sdlh/common/StringUtilLH.java
+++ web-common/src/main/java/com/sdlh/common/StringUtilLH.java
@@ -1,5 +1,7 @@
 package com.sdlh.common;
 
+import java.util.Random;
+
 public class StringUtilLH {
 	public static boolean isEmpty(String src) {
 		if(src==null || "".equals(src.trim()) || "null".equalsIgnoreCase(src)) {
@@ -14,4 +16,23 @@ public class StringUtilLH {
 		}
 		return false;
 	}
+	
+	public static String getStringRandom(int length) {  
+
+        String val = "";  
+        Random random = new Random();        
+        //length为几位密码 
+        for(int i = 0; i < length; i++) {          
+            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";  
+            //输出字母还是数字  
+            if( "char".equalsIgnoreCase(charOrNum) ) {  
+                //输出是大写字母还是小写字母  
+                int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;  
+                val += (char)(random.nextInt(26) + temp);  
+            } else if( "num".equalsIgnoreCase(charOrNum) ) {  
+                val += String.valueOf(random.nextInt(10));  
+            }  
+        }  
+        return val;  
+    }
 }
